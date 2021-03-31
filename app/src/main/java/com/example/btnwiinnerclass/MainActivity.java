c38180b412f61626cb075696df2f44796b4a167f
JunHyuk <kkjun221@naver.com>
2021-03-31 12:37:51 +0900

    inner class for multiple buttons

--- app/src/main/java/com/example/btnwiinnerclass/MainActivity.java
+++ app/src/main/java/com/example/btnwiinnerclass/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button mbutton1;
+    Button mbutton1,mbutton2;
     TextView mTextView1;
 
     @Override
@@ -17,23 +17,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mbutton1 = findViewById(R.id.button1);
+        mbutton2 = findViewById(R.id.button2);
         mTextView1 = findViewById(R.id.textView1);
 
-        mbutton1.setOnClickListener(new MyOnClickListener(this));
+        mbutton1.setOnClickListener(new MyOnClickListener());
+        mbutton2.setOnClickListener(new MyOnClickListener2());
     }
 
     private class MyOnClickListener implements View.OnClickListener {
-        MainActivity mainActivity;
-
-        public MyOnClickListener(MainActivity mainActivity) {
-            this.mainActivity = mainActivity;
-        }
-
-
         @Override
         public void onClick(View v) {
-            mainActivity.mTextView1.setText("you clicked button 1");
+           mTextView1.setText("you clicked button 1");
 
+        }
+
+    }
+
+    private class MyOnClickListener2 implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            mTextView1.setText("you clicked button 2");
         }
     }
 }
