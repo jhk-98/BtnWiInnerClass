593f2e6c7727202d1ea4a2405b5d3778c77d2c41
JunHyuk <kkjun221@naver.com>
2021-03-31 11:02:27 +0900

    listener class is a separate file

--- app/src/main/java/com/example/btnwiinnerclass/MainActivity.java
+++ app/src/main/java/com/example/btnwiinnerclass/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.btnwiinnerclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button mbutton1;
+    TextView mTextView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mbutton1 = findViewById(R.id.button1);
+        mTextView1 = findViewById(R.id.textView1);
+
+        mbutton1.setOnClickListener(new MyOnClickListener(this));
     }
 }
